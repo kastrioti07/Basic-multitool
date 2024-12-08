a405006bcf0e46749648303b08fe8abb3a21989d
E4G1_07 <ergipashaj96@gmail.com>
2024-12-08 19:19:23 +0100

    Add files via upload

---
+++ troll.bat
@@ -0,0 +1,48 @@
+@echo off
+MODE 50, 20
+chcp 65001 >nul
+title yall dumb af aka retarded
+color 0f
+:Yall Skids got me crackin the fuck uppppp
+
+echo try to skid it 
+ping localhost -n 2 >nul
+echo try to skid it 
+ping localhost -n 2 >nul
+echo try to skid it 
+ping localhost -n 2 >nul
+echo try to skid it 
+ping localhost -n 2 >nul
+echo try to skid it 
+ping localhost -n 2 >nul
+echo [38;2;255;182;193mtry to skid it 
+ping localhost -n 4 >nul
+cls
+echo at this point i want yall to [91mLMAOOO
+ping localhost -n 4 >nul
+cls
+echo [38;2;255;182;193mlets see who can skid my tool the best
+ping localhost -n 4 >nul
+cls
+echo [91mfuckin losers LMAOOOO
+ping localhost -n 4 >nul
+cls
+MODE 90, 20
+echo [38;2;255;182;193mact like yall didnt just want me to release my sources like everything else lolll
+ping localhost -n 4 >nul
+echo never say [91mrat [38;2;255;182;193munless you know what your talking about because [92m99% [38;2;255;182;193mof you
+ping localhost -n 6 >nul
+cls
+MODE 50, 20
+echo have
+ping localhost -n 3 >nul
+cls
+echo have no
+ping localhost -n 3 >nul
+cls
+echo have no fuckin
+ping localhost -n 3 >nul
+cls
+echo have no fuckin clue
+ping localhost -n 5 >nul
+cls
